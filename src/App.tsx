a6a8ba8d03382ec9c40794f57ac94a7599aa8d4e
Aleks Szanowski <117806276+1szanowski@users.noreply.github.com>
2024-07-11 20:32:22 +0200

    css updates, header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Inventory from './components/Inventory';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Inventory from "./components/inventory/Inventory";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <div className='inventory-wrapper'>
-      <Inventory/>
+      <div className="header-wrapper">
+        <Header />
       </div>
-     
+
+      <div className="inventory-wrapper">
+        <Inventory />
+      </div>
     </div>
   );
 }
